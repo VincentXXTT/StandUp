a36bf3c5a639376af0fef4ab904622b2f3a4be00
Ting Xiao <ting.xiao@thoughtworks.com>
2024-05-09 14:43:29 +0800

    init test shell

--- omni_stand_up.sh
+++ omni_stand_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 定义配置名字列表
-names=("陈晗" "刘入菲" "刘文宾" "陶星蒙" "肖亭" "岳潇逸" "祝赫" "许俊")
+names=("陈晗" "刘入菲" "刘文宾" "陶星蒙" "肖亭" "岳潇逸" "喻唅" "祝赫" "许俊")
 
 # 获取当前配置名字的索引
 index=$(cat /tmp/current_index_s 2>/dev/null)
@@ -8,19 +8,19 @@ index=${index:-0}
 
 # 获取当前配置名字
 current_name=${names[$index]}
-date_time=$(date +"%Y-%m-%d %H:%M:%S")
+wechat_url="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=71196606-1470-4a4e-bf98-3d268c31987b"
 
 # 构建 JSON 字符串
 json='{
         "msgtype": "text",
         "text": {
-            "content": "当前时间是：'$date_time'，stand up时间到了，今日host是'$current_name'",
+            "content": "stand up时间到了，今日host是'$current_name'",
             "mentioned_list": ["@all"]
         }
     }'
 
 # 执行命令，并将 JSON 字符串传递给 curl
-curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=71196606-1470-4a4e-bf98-3d268c31987b' \
+curl ''$wechat_url'' \
    -H 'Content-Type: application/json' \
    -d "$json"
 
