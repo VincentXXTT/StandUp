68c7957f88d433446682ebb8db63bbebf580704d
Ting Xiao <ting.xiao@thoughtworks.com>
2024-05-09 15:45:30 +0800

    init test shell 3

--- omni_stand_up.sh
+++ omni_stand_up.sh
@@ -3,7 +3,7 @@
 names=("陈晗" "刘入菲" "刘文宾" "陶星蒙" "肖亭" "岳潇逸" "喻唅" "祝赫" "许俊")
 
 # 获取当前配置名字的索引
-index=$(cat /tmp/current_index_s 2>/dev/null)
+index=$(cat current_index.txt 2>/dev/null)
 index=${index:-0}
 
 # 获取当前配置名字
@@ -24,4 +24,7 @@ curl 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=71196606-1470-4a4e-bf
 # 更新索引，下次使用下一个配置名字
 index=$((($index + 1) % ${#names[@]}))
 echo $index > /tmp/current_index_s
+
+# 将索引值写入项目根目录下的 current_index.txt 文件中
+echo $index > current_index.txt
 
